34d988ef7c19638948f39e58d1c93fd196d4bddc
hiyohiyo <hiyohiyo@crystalmark.info>
2024-08-17 09:38:48 +0900

    - Added Windows 9x/NT/2000 support - Added Darkmode Menu support

--- Src/Priscilla/SystemInfoFx.h
+++ Src/Priscilla/SystemInfoFx.h
@@ -9,13 +9,13 @@
 
 #include <winnt.h>
 
-
 //------------------------------------------------
 //   System Info
 //------------------------------------------------
 
-void GetCpuInfo(CString& cpuInfo, int* cores, int* threads);
+void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int* threads);
 void GetGpuInfo(CString& gpuInfo);
 void GetBaseBoardInfo(CString& baseBoardInfo);
 void GetComputerSystemInfo(CString& computerSystemInfo);
-void GetScreenInfo(CString& screenInfo);
+void GetScreenInfo(CString& screenInfo, int* width, int* height, int* color, CString& smoothing);
+void GetMemoryInfo(CString& screenInfo, int* size = NULL);
