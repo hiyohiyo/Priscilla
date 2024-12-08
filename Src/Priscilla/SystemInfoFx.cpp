61529e59bb234788d85fb0bc00263b744de8f225
hiyohiyo <hiyohiyo@crystalmark.info>
2024-12-08 10:19:14 +0900

    - Improved Win9x/NT support

--- Src/Priscilla/SystemInfoFx.cpp
+++ Src/Priscilla/SystemInfoFx.cpp
@@ -199,12 +199,6 @@ void getProcessorBrandString(char* brandString)
 
 void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int* threads)
 {
-
-#if _MSC_VER <= 1310
-	if (!IsWin9x())
-	{
-#endif
-
 	CString query = _T("Select * from Win32_Processor");
 
 	IWbemLocator*			pIWbemLocator = NULL;
@@ -241,9 +235,17 @@ void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int*
 							// UINT32 cores = 0;
 							// UINT32 threads = 0;
 
+#if defined(_M_IX86) || defined(_M_X64)
+							char brandString[49] = { 0 };
+							getProcessorBrandString(brandString);
+							name = brandString;
+							name.TrimLeft();
+							name.TrimRight();
+							cpuName = name;
+#endif
 							VARIANT pVal;
 							VariantInit(&pVal);
-							if (pCOMDev->Get(L"Name", 0L, &pVal, NULL, NULL) == WBEM_S_NO_ERROR && pVal.vt > VT_NULL)
+							if (name.IsEmpty() && pCOMDev->Get(L"Name", 0L, &pVal, NULL, NULL) == WBEM_S_NO_ERROR && pVal.vt > VT_NULL)
 							{
 								name = pVal.bstrVal;
 								name.TrimLeft();
@@ -325,19 +327,23 @@ void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int*
 	SAFE_RELEASE(pIWbemServices);
 	SAFE_RELEASE(pIWbemLocator);
 
-#if _MSC_VER <= 1310
-	}
-#endif
-
 	if (cpuInfo.IsEmpty())
 	{
-		TCHAR str[256];
+		TCHAR str[256] = {};
 		DWORD value = 0;
 		DWORD type = REG_SZ;
 		ULONG size = 256 * sizeof(TCHAR);
 		HKEY  hKey = NULL;
 
-		if (! IsWin9x())
+#if defined(_M_IX86) || defined(_M_X64)
+		char brandString[49] = { 0 };
+		getProcessorBrandString(brandString);
+		cpuInfo = brandString;
+		cpuInfo.TrimLeft();
+		cpuInfo.TrimRight();
+#endif
+
+		if (cpuInfo.IsEmpty() && !IsWin9x())
 		{
 			if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, _T("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0"), 0, KEY_READ, &hKey) == ERROR_SUCCESS)
 			{
@@ -349,17 +355,6 @@ void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int*
 				}
 			}
 		}
-
-#if defined(_M_IX86) || defined(_M_X64)
-		if (cpuInfo.IsEmpty())
-		{
-			char brandString[48] = { 0 };
-			getProcessorBrandString(brandString);
-			cpuInfo = brandString;
-			cpuInfo.TrimLeft();
-			cpuInfo.TrimRight();
-		}
-#endif
 
 #ifdef _M_IX86
 		if (cpuInfo.IsEmpty())
@@ -422,6 +417,12 @@ void GetCpuInfo(CString& cpuInfo, CString& cpuName, int* clock, int* cores, int*
 			cpuInfo += cstr;
 		}		
 	}
+
+
+	cpuName.Replace(_T("(R)"), _T(""));
+	cpuName.Replace(_T("(TM)"), _T(""));
+	cpuInfo.Replace(_T("(R)"), _T(""));
+	cpuInfo.Replace(_T("(TM)"), _T(""));
 }
 
 void GetGpuInfo(CString& gpuInfo)
@@ -452,7 +453,7 @@ void GetGpuInfo(CString& gpuInfo)
 				pAdapter->GetDesc(&adapterDesc);
 
 				CString cstr;
-				cstr.Format(L"%s (%d MB)\n", adapterDesc.Description, (int)(adapterDesc.DedicatedVideoMemory / (1024 * 1024)));
+				cstr.Format(L"%s [%d MB]", adapterDesc.Description, (int)(adapterDesc.DedicatedVideoMemory / (1024 * 1024)));
 
 				if (cstr.Find(L"Microsoft Basic Render Driver") == 0)
 				{
@@ -476,6 +477,8 @@ void GetGpuInfo(CString& gpuInfo)
 
 	if (! gpuInfo.IsEmpty())
 	{
+		gpuInfo.Replace(_T("(R)"), _T(""));
+		gpuInfo.Replace(_T("(TM)"), _T(""));
 		return;
 	}
 #endif
@@ -551,7 +554,7 @@ void GetGpuInfo(CString& gpuInfo)
 
 	if (gpuInfo.IsEmpty())
 	{
-		TCHAR str[256];
+		TCHAR str[256] = {};
 		DWORD value = 0;
 		DWORD type = REG_SZ;
 		ULONG size = 256 * sizeof(TCHAR);
@@ -586,6 +589,9 @@ void GetGpuInfo(CString& gpuInfo)
 			}
 		}
 	}
+
+	gpuInfo.Replace(_T("(R)"), _T(""));
+	gpuInfo.Replace(_T("(TM)"), _T(""));
 }
 
 void GetBaseBoardInfo(CString& baseBoardInfo)
@@ -665,7 +671,7 @@ void GetBaseBoardInfo(CString& baseBoardInfo)
 
 	if (baseBoardInfo.IsEmpty())
 	{
-		TCHAR str[256];
+		TCHAR str[256] = {};
 		DWORD value = 0;
 		DWORD type = REG_SZ;
 		ULONG size = 256 * sizeof(TCHAR);
@@ -777,7 +783,7 @@ void GetComputerSystemInfo(CString& computerSystemInfo)
 
 	if (computerSystemInfo.IsEmpty())
 	{
-		TCHAR str[256];
+		TCHAR str[256] = {};
 		DWORD value = 0;
 		DWORD type = REG_SZ;
 		ULONG size = 256 * sizeof(TCHAR);
@@ -809,6 +815,13 @@ void GetComputerSystemInfo(CString& computerSystemInfo)
 			RegCloseKey(hKey);
 		}
 	}
+
+#if _MSC_VER <= 1310
+	if (IsPC98())
+	{
+		computerSystemInfo = _T("[PC-98] ") + computerSystemInfo;
+	}
+#endif
 }
 
 void GetScreenInfo(CString& screenInfo, int* width, int* height, int* color, CString& smoothing)
@@ -844,7 +857,7 @@ void GetScreenInfo(CString& screenInfo, int* width, int* height, int* color, CSt
 void GetMemoryInfo(CString& memoryInfo, int* size)
 {
 #if _MSC_VER > 1310
-	MEMORYSTATUSEX memory;
+	MEMORYSTATUSEX memory = {};
 	memory.dwLength = sizeof(memory);
 	GlobalMemoryStatusEx(&memory);
 
